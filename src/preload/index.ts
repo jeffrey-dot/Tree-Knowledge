f9a22b40bed28eead3ac257f7047c6240b575280
jeffrey <jeffrey.rscoder@gmail.com>
2026-04-17 18:00:10 +0800

    chore: remove app scaffold and keep docs only

--- src/preload/index.ts
+++
@@ -1,9 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron'
-
-const api = {
-  ping: () => ipcRenderer.invoke('ping'),
-}
-
-contextBridge.exposeInMainWorld('api', api)
-
-export type Api = typeof api
